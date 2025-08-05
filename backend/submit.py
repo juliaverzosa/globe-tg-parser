14a8a6938f32185003546ebb8a7c87b7e5970d67
juliaverzosa <j.verzosa.534381@umindanao.edu.ph>
2025-08-05 11:14:38 +0800

    debug

--- backend/submit.py
+++ backend/submit.py
@@ -7,7 +7,7 @@ from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://globe-tg-parser.vercel.app/"}})
+CORS(app)
 
 
 SPREADSHEET_ID = '1jk-hvI6K-DABDtT1hjFdo-dqccUM_jINDv1zhRDBlnk'
